20a2f18db5872fe0993e984dfcfb051423a399dd
arrisairdiyana-dotcom <arrisairdiyana@gmail.com>
2026-06-30 15:04:39 +0800

    Complete exercise 1-3

---
+++ labs/lab03/exercise2/lab-03-2.py
@@ -0,0 +1,4 @@
+numNight = int(input())
+totalPayment = numNight * 250 * 1.15
+print(totalPayment)
+
---
+++ labs/lab03/exercise3/lab-03-3.py
@@ -0,0 +1,9 @@
+kilobyte = int(input())
+megabyte = float(kilobyte) / 1024
+print(megabyte)
+gigabyte = float(megabyte) / 1024
+print(gigabyte)
+terabyte = float(gigabyte) / 1024
+print(terabyte)
+petabyte = float(terabyte) / 1024
+print(petabyte)
