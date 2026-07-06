6ce2b4a1df96f01b052004e0e57bab587f02295d
arrisairdiyana-dotcom <arrisairdiyana@gmail.com>
2026-07-06 09:35:20 +0800

    completemlab-03

---
+++ labs/lab03/exercise6/lab-03-6.py
@@ -0,0 +1,7 @@
+yardLength = float(input())
+yardWidth = float(input())
+houseLength = float(input())
+houseWidth = float(input())
+shadedArea = yardLength * yardWidth - houseLength * houseWidth
+wage = shadedArea * 2
+print(wage)
---
+++ labs/lab03/exercise7/lab-03-7.py
@@ -0,0 +1,5 @@
+celsius = float(input())
+fahrenheit = celsius * 9 / 5 + 32
+print(fahrenheit)
+kelvin = celcius + 273.15
+print(kelvin)
