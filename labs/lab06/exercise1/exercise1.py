d42f2badf424013ee559ea65b4a48eb79282d375
FiffyD <fiffydaphnyee@gmail.com>
2026-08-10 08:37:59 +0800

    ex1 lab6

--- labs/lab06/exercise1/exercise1.py
+++ labs/lab06/exercise1/exercise1.py
@@ -1,4 +1,22 @@
-# Escape Characters Exercise
-# Print the receipt shown in the lab, using \n for new lines and \t for columns.
-# Calculate every total, subtotal, and tax in your code. Do not type the money
-# amounts in directly. Show every amount with exactly two decimal places.
+item1 = "Coffee"
+price1 = 3.50
+quantity1 = 2
+
+item2 = "Muffin"
+price2 = 2.10
+quantity2 = 3
+
+item3 = "water"
+price3 = 1.05
+quantity3 = 4
+
+total1= price1*quantity1
+total2= price2*quantity2
+total3= price3*quantity3
+
+subtotal= total1+total2+total3
+tax= subtotal*0.06
+total=subtotal+tax
+
+print (
+    f"========== RECEIPT ==========\nItem\tPrice\tQty\tTotal\n{item1}\t{price1}\t{quantity1}\t{total1}\n{item2}\t{price2}\t{quantity2}\t{total2}\n{item3}\t{price3}\t{quantity3}\t{total3}\n------------------------------\nSubtotal\t\t${subtotal}\nTax(6%)\t\t\t${tax}\nTotal\t\t{total}\n============================")
