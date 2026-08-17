eab1d3fb01f2b4a8b8a78adad75b270060e3d6a2
FiffyD <fiffydaphnyee@gmail.com>
2026-08-17 08:23:46 +0800

    lab 07

---
+++ labs/lab06/exercise1/lab-test-2.py
@@ -0,0 +1,10 @@
+#Lab test 02 by Luqman Al Hakim C02
+# Write a complete python program that uses an appropriate escape characters and arithmetic expression to produce the output as below:
+
+#User enters name,matric number & calculate total marks
+name = ("Ahmad Bin Abu")
+matric_no = ("MS2025123499")
+total_marks= 4+8+8
+
+#Prints user's name,matric number and shows full marks
+print (f"Name: {name}\t\t\t\t\tMatric. No: {matric_no}\n\n*\t\t\t*\n**\t\t**\n***\t***\n********\n***\t***\n**\t\t**\n*\t\t\t*\n\nThis is my \n\tsecond\n\t\tassignment\n I want 2x10 marks,which is {total_marks} full marks\n")
