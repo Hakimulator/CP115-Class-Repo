eadae204e2a5c7f32972b25d72b210efa18a9de2
Hakimulator <luqman.ahhassan@gmail.com>
2026-09-14 09:34:18 +0800

    Lab 08 ex 1-5 completed

--- labs/lab08/exercise5/exercise5.py
+++ labs/lab08/exercise5/exercise5.py
@@ -2,6 +2,22 @@ main_course = input()
 drink = input()
 dessert = input()
 
+if main_course== "Chicken":
+    price1= 10
+elif main_course == "Beef":
+    price1 = 12
+else:
+    price1 = 11
+if drink == "Soft Drink":
+    price2 = 2
+else:
+    price2 = 3
+if dessert == "Ice Cream":
+    price3=4
+else:
+    price3=5
 
+service_charge= (price1+price2+price3)*0.1
+final_bill= price1+price2+price3+service_charge
 
 print(f"{final_bill:.2f}")
